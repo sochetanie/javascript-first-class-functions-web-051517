06ee8b1de74c2873636cfe3d5ef6ddffab042525
sochetanie <allagorik2901@gmail.com>
2017-06-27 23:35:02 -0400

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback) {
+    callback()
+}
+
+function returnsANamedFunction() {
+    return function name() {
+
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        
+    }
+}
